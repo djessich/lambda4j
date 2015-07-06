09da8f2cfb7e353b2b9e0b3cf4bca379d1f7159b
JESSICH Dominik <if12b094@technikum-wien.at>
2015-07-07 00:27:08 +0200

    Updated package-info.java

--- throwable/package-info.java
+++ throwable/package-info.java
@@ -42,9 +42,9 @@
  * declaration in the <em>throws</em> clause. The checked exception will behave just like a normal <b>unchecked</b>
  * exception due to sneaky throwing.
  * <p>
- * A throwable Lambda inherits its Java 8, non-throwable equivalent. This means, that for example {@link
+ * A throwable Lambda inherits from its non-throwable equivalent. This means, that for example {@link
  * at.gridtec.internals.lang.function.throwable.ThrowableFunction} inherits from {@link java.util.function.Function}.
- * So each throwable Lambda is usable as its Java 8 counterpart. For example:
+ * So each throwable Lambda is usable as its non-throwing counterpart. For example:
  * <pre>
  *     final List&lt;String&gt; stringList = new ArrayList&lt;String&gt;();
  *     // ... populate list ...
@@ -52,8 +52,34 @@
  *     List&lt;Class&lt;?&gt;&gt; result = stringList.stream().map(mapper).collect(Collectors.toList());
  * </pre>
  * <p>
- * Thereby any throwable lambda include some default methods. Those can be used to react to thrown exceptions. For
- * example we are able to execute some other lambda if the first one fails.
+ * To avoid unnecessary casting of a non-throwable to a throwable lambda (anonymous), each throwable one does provide a
+ * wrapping method to implicitly cast the non-throwable. In some cases the throwable instance is ambiguous for the
+ * compiler, which might happen for overloaded methods accepting different functional interfaces. For example:
+ * <pre>
+ *     final List&lt;String&gt; stringList = new ArrayList&lt;String&gt;();
+ *     // ... populate list ...
+ *     List&lt;Class&lt;?&gt;&gt; result = stringList.stream()
+ *             .map(Throwable.wrap(Class::forName)) // implicit cast using wrap method
+ *             .collect(Collectors.toList());
+ * </pre>
+ * <p>
+ * Also mapping from a non-throwable instance to a throwable one is possible. Each throwable lambda provides a method
+ * for this case. The following example demonstrates such a case and assumes an old-style error handling lambda, which
+ * is a lambda catching the appropriate exception and throwing an unchecked exception instead of the original one.
+ * Obviously, the method used in this case is just for convenience.
+ * <pre>
+ *     final Function&lt;String, Class&lt;?&gt;&gt; function = str -&gt; {
+ *         try {
+ *             return Class::forName;
+ *         } catch (ClassNotFoundException e) {
+ *             throw new RuntimeException(e);
+ *         }
+ *     };
+ *     ThrowableFunction&lt;String, Class&lt;?&gt;&gt; throwableFunction = ThrowableFunction.from(function);
+ * </pre>
+ * <p>
+ * Moreover each throwable lambda, include some default methods. Those can be used to react to thrown exceptions. For
+ * example we are able to execute some other throwable lambda if the first one fails.
  * <pre>
  *     final List&lt;String&gt; stringList = new ArrayList&lt;String&gt;();
  *     // ... populate list ...
