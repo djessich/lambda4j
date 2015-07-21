21d5726ea42db3daf78b94d974d4773cc4d55eb1
JESSICH Dominik <if12b094@technikum-wien.at>
2015-07-21 22:03:50 +0200

    Moved comparator package

--- src/main/java/at/gridtec/lambda4j/comparator/ThrowableComparator.java
+++ src/main/java/at/gridtec/lambda4j/comparator/ThrowableComparator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package at.gridtec.lambda4j.throwable.comparator;
+package at.gridtec.lambda4j.comparator;
 
 import at.gridtec.lambda4j.util.ThrowableUtils;
 
